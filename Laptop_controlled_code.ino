2fcba50873c698cc8383a33a166632018248a4a3
shristiuniyal <44426078+shristiuniyal@users.noreply.github.com>
2020-01-13 04:56:41 +0530

    Add files via upload

---
+++ Laptop_controlled_code.ino
@@ -0,0 +1,126 @@
+#include <Servo.h>
+
+
+int motorL[]={3,2};  
+int motorR[]={5,4};
+int movement;
+int flag=0;
+int angle;
+Servo upDownServo;
+Servo clawServo;
+
+void setup() 
+{
+  Serial.begin(9600);
+  upDownServo.attach(9);
+  clawServo.attach(10); 
+  pinMode(motorL[0],OUTPUT);
+  pinMode(motorL[1],OUTPUT);
+  pinMode(motorR[0],OUTPUT);
+  pinMode(motorR[1],OUTPUT); 
+  Serial.println("How to use:-\nW-Forward \tS-Backwards\tA-Left\tD-Right\tQ-Arm Up\tZ-Arm Down\tE-Claw Open\tC-Claw Close");
+}   
+
+
+
+void loop() 
+{   
+    
+     if (Serial.available())
+
+      {      movement= Serial.read();
+      
+            if(movement== int('W')||movement==int('w'))
+               flag=1;
+             
+            else if(movement== int('A')||movement==int('a'))
+               flag=2;
+            
+            else if(movement== int('D')||movement==int('d'))
+               flag=3;
+            
+            else if(movement== int('S')||movement==int('s'))
+               bck();
+            else if(movement== int('Q')||movement==int('q'))
+               up();
+            else if(movement== int('Z')||movement==int('z'))
+               down();
+            else if(movement== int('E')||movement==int('E'))
+               clawOpen();
+            else if(movement== int('C')||movement==int('C'))
+               clawClose();
+            else
+               nomove();
+      }
+}  
+
+    
+
+
+
+void fwd()
+
+{ 
+  Serial.println("Moving Forward");
+  digitalWrite(motorR[0],HIGH);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],HIGH);
+  digitalWrite(motorL[1],LOW);
+
+}
+void bck()
+{ 
+  Serial.println("Moving Backward");
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],HIGH);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],HIGH);
+
+}
+void right()
+{
+  Serial.println("Moving Right");
+  digitalWrite(motorR[0],HIGH);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],HIGH);
+
+}
+
+void left()
+{
+  Serial.println("Moving left");
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],HIGH);
+  digitalWrite(motorL[0],HIGH);
+  digitalWrite(motorL[1],LOW);
+
+}
+
+void up(){
+  Serial.println("Moving arm up");
+  upDownServo.write(120);
+}
+
+void down(){
+  Serial.println("Moving arm down");
+  upDownServo.write(180);
+}
+
+void clawClose(){
+  Serial.println("Closing the claw");
+  clawServo.write(90);
+}
+
+void clawOpen(){
+  Serial.println("Open the claw");
+  clawServo.write(30);
+}
+
+void nomove()
+{
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],LOW);
+}
---
+++ dtmf_controlled_code.ino
@@ -0,0 +1,97 @@
+
+int motorL[]={3,2};  
+int motorR[]={5,4}; 
+int VT=7;
+int Q4=8;       
+int Q3=9;       
+int Q2=10;      
+int Q1=11;      
+int r1;         
+int r2;
+int r3;
+int r4;
+void setup()                                              
+{   Serial.begin(9600);
+    for(int i=0;i<2;i++)
+    {   pinMode(motorL[i],OUTPUT);
+        pinMode(motorR[i],OUTPUT);
+    }
+    pinMode(Q4,INPUT); 
+    pinMode(Q3,INPUT);
+    pinMode(Q2,INPUT);
+    pinMode(Q1,INPUT);
+    r1=0;
+    r2=0;
+    r3=0;
+    r4=0;
+}        
+void loop()                                                                                   
+{   int read[]={digitalRead(Q4),digitalRead(Q3),digitalRead(Q2),digitalRead(Q1)};                
+    //r2=digitalRead(Q3);
+    //r3=digitalRead(Q2);
+    //r4=digitalRead(Q1);   
+   
+            if(read[0]==0&&read[1]==0&&read[2]==1&&read[3]==0)       
+            { Serial.println("Moving Forward");
+              fwd();
+            }
+           else if(read[0]==0&&read[1]==1&&read[2]==0&&read[3]==0)        
+            { Serial.println("Moving Left");
+              left();
+            }
+            else if(read[0]==0&&read[1]==1&&read[2]==0&&read[3]==1)      
+            { Serial.println("Not moving");
+              nomove();
+            }
+            else if(read[0]==0&&read[1]==1&&read[2]==1&&read[3]==0)    
+            { Serial.println("Moving Right");
+              right();
+            }
+            else if(read[0]==1&&read[1]==0&&read[2]==0&&read[3]==0)      
+            { Serial.println("Moving Backward");
+              bck();
+            }
+              
+}              
+void fwd()
+
+{ 
+  digitalWrite(motorR[0],HIGH);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],HIGH);
+  digitalWrite(motorL[1],LOW);
+
+}
+void bck()
+{ 
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],HIGH);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],HIGH);
+
+}
+void left()
+{
+  digitalWrite(motorR[0],HIGH);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],HIGH);
+
+}
+
+void right()
+{
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],HIGH);
+  digitalWrite(motorL[0],HIGH);
+  digitalWrite(motorL[1],LOW);
+
+}
+
+void nomove()
+{
+  digitalWrite(motorR[0],LOW);
+  digitalWrite(motorR[1],LOW);
+  digitalWrite(motorL[0],LOW);
+  digitalWrite(motorL[1],LOW);
+}
